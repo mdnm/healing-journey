90f006d81fec957ece533b9a73c649f3dd94bdd1
Mateus De Nardo <mateusdnm@hotmail.com>
2023-10-07 19:35:36 +0200

    fix: fix for first deploy

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
       "logos-world.net",
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/plausible/js/script.js",
+        destination: "https://plausible.io/js/script.js",
+      },
+      {
+        source: "/plausible/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
