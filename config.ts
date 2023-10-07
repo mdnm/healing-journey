54a02ab26e107b5a85a178388afac6e8b0b1e282
Mateus De Nardo <mateusdnm@hotmail.com>
2023-10-07 19:31:50 +0200

    fix: fix for first deploy

--- config.ts
+++ config.ts
@@ -11,7 +11,7 @@ const config = {
   domainName: "numerosapp.com",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
-    id: "",
+    id: "c9b43bf6-fe78-4830-a385-6d7e1aa0d8bd",
     // Hide Crisp by default, except on route "/". Crisp is toggled with <ButtonSupport/>. If you want to show Crisp on every routes, just remove this below
     onlyShowOnRoutes: ["/"],
   },
@@ -75,13 +75,13 @@ const config = {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `ShipFast <noreply@mg.shipfa.st>`,
+    fromNoReply: `NumEros <mateus.nardo@migii.com.br>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Marc at ShipFast <marc@mg.shipfa.st>`,
+    fromAdmin: `Mateus at NumEros <mateus.nardo@migii.com.br>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: "marc@mg.shipfa.st",
+    supportEmail: "",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
-    forwardRepliesTo: "marc.louvion@gmail.com",
+    forwardRepliesTo: "",
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to _document.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
--- models/User.ts
+++ models/User.ts
@@ -2,7 +2,6 @@ export default interface User {
   id: string;
   name?: string;
   email?: string;
-  image?: string;
   customerId?: string;
   priceId?: string;
   createdAt?: Date;
