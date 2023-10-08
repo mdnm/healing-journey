b8c379fc196c918be6100d33a35c7f10d889959f
Mateus De Nardo <mateusdnm@hotmail.com>
2023-10-08 23:43:11 +0200

    fix: fix copy

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-          Decodifique sua realidade
+          Melhore seus relacionamentos, amizades, trabalho e finanças
         </h1>
         <p className="text-lg opacity-80 leading-relaxed">
           Entenda porque você parece estar andando contra a maré ou repetindo os
-          mesmos erros nos relacionamentos, trabalho e saúde e como mudar isso.
+          mesmos erros e como mudar isso.
         </p>
         <Link href={"/#pricing"}>
           <ButtonGradient title="Quero uma leitura" />
