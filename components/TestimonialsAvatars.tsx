f77de4228d129e332c50c7271e23011df315baaf
Mateus De Nardo <mateusdnm@hotmail.com>
2023-10-30 19:51:56 +0100

    feat: make basic reading basic

--- components/TestimonialsAvatars.tsx
+++ components/TestimonialsAvatars.tsx
@@ -71,7 +71,7 @@ const TestimonialsAvatars = ({ priority }: { priority?: boolean }) => {
         </div>
 
         <div className="text-base text-base-content/80">
-          <span className="font-semibold text-base-content">+1300</span>{" "}
+          <span className="font-semibold text-base-content">+2000</span>{" "}
           leituras básicas feitas
         </div>
       </div>
