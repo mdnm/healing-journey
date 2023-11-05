ac3947f9e6a66e381d814db8d3ba1334c9417016
Mateus De Nardo <mateusdnm@hotmail.com>
2023-11-05 18:44:47 +0100

    chore: make adjustments

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -35,7 +35,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
 
       {/* Add Whatsapp Button */}
       <a
-        href="https://wa.me/5511988394372"
+        href="https://wa.me/393668717788"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-50"
